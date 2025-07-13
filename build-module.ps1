bf79ede81502d115789137aeff40fbb36fce7277
James Barwick <jbarwick@me.com>
2025-07-13 22:49:47 +0800

    New global build script

--- build-module.ps1
+++ build-module.ps1
@@ -1,53 +1,5 @@
 # Powershell
 
-# take in one parameter called "type" and default to "module"
-param (
-    [string]$type = "module"
-)
-
-Write-Host "Building module type: $type"
-
-# if the file pyproject.toml does not exist, return with error "Must be in project folder":
-if (-not (Test-Path -Path "./pyproject.toml" -PathType Leaf)) {
-    Write-Host "Must be in project folder"
-    return
-}
-
-# deactivate virutal enivonrment if active
-if ($env:VIRTUAL_ENV) {
-    Write-Host "Deactivating virtual environment"
-    deactivate
-}
-
-# Remove the .venv folder if it exists
-if (Test-Path -Path ".venv" -PathType Container) {
-    Write-Host "Removing virtual environment"
-    Remove-Item -Path ".venv" -Recurse -Force
-}
-
-# if the .venv folder doesn't exist, create  with python -m venv .venv
-if (-not (Test-Path -Path ".venv" -PathType Container)) {
-    Write-Host "Creating virtual environment"
-    python -m venv .venv
-}
-
-# Activate the virtual environment
-Write-Host "Activating virtual environment"
-. .venv/Scripts/Activate.ps1
-
-Write-Host "We want python $version"
-Write-Host "Active python version is: $(python --version)"
-
-Write-Host "`nUpdating Poetry dependencies"
-python -m pip -q install --upgrade pip
-python -m pip -q install --upgrade poetry poetry-dynamic-versioning polib
-poetry self add -q poetry-plugin-export
-
-# Remove poetry.lock if it exists (this is temporary)
-if (Test-Path -Path "poetry.lock" -PathType Leaf) {
-    Remove-Item -Path "poetry.lock" -Force
-}
-
 # Source and execute the build script
 . ..\build.ps1
 
@@ -59,9 +11,3 @@ if (Test-Path -Path "poetry.lock" -PathType Leaf) {
 
 # Source and execute the publish script
 . ..\publish.ps1
-
-if ($type -eq "lambda") {
-
-    # Source and execute the package script
-    . ..\package.ps1
-}
--- build-module.sh
+++ build-module.sh
@@ -1,44 +1,13 @@
 #!/bin/bash
 
 # if the paremter $1 is empty then default to "module"
-type=${2:-module}
 version=${4:-3.13.1}
 clean=${5}
-
-echo "Building module type: $type"
 
 # if the file pyproject.toml does not exist, return with error "Must be in project folder":
 if [ ! -f "pyproject.toml" ]; then
     echo "Must be in project folder"
     exit 1
-fi
-
-# Deactivarte the virtual environment if it is active
-if [ ! "$VIRTUAL_ENV" = "" ]; then
-   echo "Deactivating virtualenv"
-   deactivate
-fi
-
-if [ "$clean" = "-clean" ]; then
-   echo "Removing current virtualenv"
-   if [ -d .venv ]; then
-      rm -rf .venv
-   fi
-fi
-
-if [ ! -d .venv ]; then
-   echo "Creating new virtualenv"
-   python -mvenv .venv
-fi
-
-echo "Activating virtualenv"
-source .venv/bin/activate
-
-# If the variable $client is equal to "-clean" then clear poetry caches
-if [ "$clean" = "-clean" ]; then
-   echo "Clearing poetry caches"
-   poetry cache clear _default_cache --all --quiet
-   poetry cache clear nexus --all --quiet
 fi
 
 # Source and execute the build script
@@ -53,8 +22,7 @@ source ../pytest.sh
 # Source and execute the publish script
 source ../publish.sh
 
-if [ "$type" = "lambda" ]; then
-
-    # Source and execute the package script
+# if the file .lambda exists, then it is a lambda module
+if [ -f ".lambda" ]; then
     source ../package.sh
 fi
